acaeb4b9853201fb8028cc5397cadad8d0fd2198
parnasos <lauragraizer@gmail.com>
2021-06-22 13:34:47 +0200

    Add npm install to web

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 // create and config server
 const server = express();
@@ -10,4 +10,8 @@ server.use(express.json());
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+server.get("/", (req, res) => {
+  res.send("Holiiiis");
 });
